cecd4045fdc0ed2db2ecd144e10bf95824dfb7d1
LUCAS MARTINEZ <132151483+LucasFrontEnd21122007@users.noreply.github.com>
2023-08-27 15:14:14 -0300

    commits do projeto
    commits do projeto de hoje

---
+++ index.js
@@ -0,0 +1,9 @@
+// Script JavaScript para o site
+
+console.log("Script carregado!");
+
+// Exemplo de interação com o usuário
+let button = document.querySelector("button");
+button.addEventListener("click", function() {
+    alert("Você clicou no botão!");
+});
